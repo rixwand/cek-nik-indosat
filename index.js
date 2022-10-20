2d1d5b8b7398f66d233ff725f35080ad3dd5a404
rixwand <riswandir461311@gmail.com>
2022-10-21 00:05:15 +0800

    change port default

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
