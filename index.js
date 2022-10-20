d98cae2a5814ddcc1f5dca947593b0e3a5ba1fe5
rixwand <riswandir461311@gmail.com>
2022-10-20 22:01:17 +0800

    fix __dirname

--- index.js
+++ index.js
@@ -8,16 +8,13 @@ import { fileURLToPath } from "url";
 import fileUpload from "express-fileupload";
 import excelToJson from "convert-excel-to-json";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
 app.set("view engine", "ejs");
 app.use(expressEjsLayouts);
-app.use("/public", express.static(__dirname + "/public"));
+app.use("public", express.static("public"));
 
 axios.defaults.withCredentials = true;
 
@@ -148,4 +145,4 @@ const check = async (data) => {
   return dump;
 };
 
-app.listen(9001, () => console.log("port listening at 9001"));
+app.listen(9001);
