fd4fb0fcbd3bf49ee1bb2b9cfd6b291d27392b47
RhizOne <71880606+rixwand@users.noreply.github.com>
2023-03-15 23:27:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, './views'))
 app.use(expressEjsLayouts);
 app.use("/public", express.static(__dirname + "/static"));
 
